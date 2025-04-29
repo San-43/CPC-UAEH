1980a875d499e67e8376427b4e3e2bbfbb39411a
San-43 <virgosmp.mx@gmail.com>
2025-04-29 08:08:53 -0600

    Add initial implementation of the Collatz conjecture solution

---
+++ La_conjetura_de_Collatz.cpp
@@ -0,0 +1,20 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve () {
+    long long n;
+    cin >> n;
+    
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t = 1;
+    while(t--) {
+        solve();
+    }
+}
