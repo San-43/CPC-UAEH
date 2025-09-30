3dd09479a5f7950a08d876ac5bcd05dea8b26e84
Sandro Martínez <virgosmp.mx@gmail.com>
2025-09-29 19:44:09 -0600

    Add complete implementation of Lento program

---
+++ Lento.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("Ofast,unroll-loops,no-stack-protector")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+
+#define sz(x) int(x.size())
+#define fi first
+#define se second
+#define pb push_back
+#define edl '\n'
+
+constexpr long long LLINF = 2e18;
+constexpr int INF = 2e9;
+constexpr int MOD = 1e9 + 7;
+constexpr int MxN = 2e5 + 5;
+constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+void solve() {
+    int n;
+    cin >> n;
+    ll ans = 0;
+    bool impar = false;
+    int m = INF;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        ans += x;
+        if (x & 1) {
+            impar = true;
+            m = min(m, x);
+        }
+    }
+    if (ans & 1)
+        cout << ans << edl;
+    else if (impar == false)
+        cout << 0 << edl;
+    else
+        cout << ans - m << edl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
