a800cfaf9748c74848daad5c2c046dc50a402066
San-43 <virgosmp.mx@gmail.com>
2025-12-01 18:19:27 -0600

    working

--- campamento_fall_2025/Contests/LastOne/Herencia.cpp
+++ campamento_fall_2025/Contests/LastOne/Herencia.cpp
@@ -20,7 +20,33 @@ constexpr int MxN = 2e5 + 5;
 constexpr int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
 
 void solve() {
-    
+    ll n, x, k;
+    cin >> n >> x >> k;
+    vector<vector<int>> a(n+5, vector<int>());
+
+    unordered_map<int, int> mp;
+
+    for(int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        mp[i] = x;
+    }
+
+    for(int i = 1; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        a[x].push_back(y);
+    }
+    int l = 0;
+    int r = a.size();
+    int nodo = 1;
+    ll tmp = 0;
+    while(l <= r) {
+        int mid = l + (r - l) / 2;
+        ll sum = 0;
+        
+    }
+
 }
 
 int main() {
